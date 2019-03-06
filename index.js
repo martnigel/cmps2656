1e427db7203338871bd6844aef77eabc12aa80b3
martnigel <48106730+martnigel@users.noreply.github.com>
2019-03-06 11:54:06 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-module.exports =
-    mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_USER_PASSWORD,
-        database: process.env.DB
-    });
